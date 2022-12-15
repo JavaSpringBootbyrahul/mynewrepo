c3d976e7488d0036589e29ceef32c3e02a99ba2a
RAHUL-PC\Rzope <rzope5923@gmail.com>
2022-12-15 18:29:00 +0530

    HIS-302 display() going to commit

--- MyFile.java
+++ MyFile.java
@@ -1,7 +1,10 @@
-public class MyFile
-{
-	public static void main(String args[])
-	{
+public class MyFile {
+	public static void main(String args[]) {
 		System.out.println("New Repo created");
+		display();
+	}
+
+	public void display() {
+		// HIS-301 task
 	}
 }
